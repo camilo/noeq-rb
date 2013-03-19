53ccefacb85239c3114a8a5707154c5abf27ff95
Tom Burns <tom.burns@jadedpixel.com>
2013-03-19 15:18:12 -0400

    Add test cases for socket/network failures from noeq server

--- test/noeq_test.rb
+++ test/noeq_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require './lib/noeq'
 
-class NoeqTest < Test::Unit::TestCase
+class NoeqSimpleTest < Test::Unit::TestCase
 
   def setup
     FakeNoeqd.start
@@ -45,32 +45,60 @@ class NoeqTest < Test::Unit::TestCase
     assert_equal expected_id, noeq.fetch_id
   end
 
-  def test_async_request_with_disconnected_server_raises
-    noeq = Noeq.new(Noeq::DEFAULT_HOST, 4444, :async => true)
-    FakeNoeqd.stop
-    assert_raises(Errno::EPIPE) { noeq.request_id }
-  end
-
-  def test_sync_request_with_unresponsive_server_after_connect_raises
-    FakeNoeqd.stop
-    FakeNoeqd.start(4444, :block_on_read => true)
-    noeq = Noeq.new(Noeq::DEFAULT_HOST, 4444)
-    assert_raises(Noeq::ReadTimeoutError){  noeq.generate }
-  end
-
   private
 
   def expected_id
     144897448664367104
   end
+end
 
+class NoeqdFailureConditionTest < Test::Unit::TestCase
+
+  def teardown
+    FakeNoeqd.stop
+  end
+
+  def test_async_request_with_disconnected_server_raises
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 4444, :async => true)
+    assert_raises(Errno::EPIPE) { noeq.request_id }
+  end
+
+  def test_sync_request_with_unresponsive_server_after_connect_raises
+    FakeNoeqd.start(5555, :action => :block_read)
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 5555)
+    assert_raises(Noeq::ReadTimeoutError){  noeq.generate }
+  end
+
+  def test_sync_request_with_disconnecting_server_after_connect_raises
+    FakeNoeqd.start(5555, :action => :disconnect_read)
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 5555)
+    assert_raises(Noeq::ReadTimeoutError){  noeq.generate }
+  end
+
+  def test_generate_when_server_never_accepts_raises
+    FakeNoeqd.start(5556, :action => :never_accept)
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 5556)
+    assert_raises(Noeq::ReadTimeoutError){  noeq.generate }
+  end
+
+  def test_generate_receiving_short_response_raises
+    FakeNoeqd.start(5557, :action => :short_write)
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 5557)
+    assert_raises(Noeq::ReadError){  noeq.generate }
+  end
+
+  def test_generate_receiving_long_response_raises
+    FakeNoeqd.start(5558, :action => :long_write)
+    noeq = Noeq.new(Noeq::DEFAULT_HOST, 5558)
+    assert_raises(Noeq::ReadError){  noeq.generate; noeq.generate }
+  end
 end
 
 class FakeNoeqd
 
   def self.start(port = 4444, options = {})
     @server = new(port, options)
-    Thread.new { @server.accept_connections }
+    @thread = Thread.new { @server.accept_connections }
   end
 
   def self.stop
@@ -84,17 +112,31 @@ class FakeNoeqd
 
   def stop
     @socket.close rescue true
+    @thread.kill rescue true
   end
 
   def accept_connections
-    while conn = @socket.accept
-      while n = conn.read(1)
-        if @options[:block_on_read]
-          sleep(1)
+    if @options[:action] == :never_accept
+      while true
+        sleep(100)
+      end
+    else
+      while conn = @socket.accept
+        while true
+          n = conn.read(1)
+          return @socket.close if @options[:action] == :disconnect_read
+          if @options[:action] == :block_read
+            sleep(100)
+          end
+          data = "\x02\x02\xC7v<\x80\x00\x00"
+          if @options[:action] == :short_write
+            data = "\x02\x02v<\x80\x00\x00"
+          elsif @options[:action] == :long_write
+            data = "\x02\x02\xC7v<\x80\x00\x00\x00"
+          end
+          conn.send data * n.unpack('c')[0], 0
         end
-        conn.send "\x02\x02\xC7v<\x80\x00\x00" * n.unpack('c')[0], 0
       end
     end
   end
-
 end
